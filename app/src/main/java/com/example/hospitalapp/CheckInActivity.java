80124dbdd2827996d2f93a3d57b752d666a60199
nnvsek <nnvsek@gmail.com>
2020-02-07 15:08:36 -0500

    back button in my app class

--- app/src/main/java/com/example/hospitalapp/CheckInActivity.java
+++ app/src/main/java/com/example/hospitalapp/CheckInActivity.java
@@ -2,15 +2,29 @@ package com.example.hospitalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckInActivity extends AppCompatActivity {
-
+Button B2;
+Intent I;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        B2= findViewById(R.id.backtomain2);
+
+        B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                I = new Intent(CheckInActivity.this, Main2Activity.class);
+                startActivity(I);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/hospitalapp/MyAppointActivity.java
+++ app/src/main/java/com/example/hospitalapp/MyAppointActivity.java
@@ -2,15 +2,30 @@ package com.example.hospitalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
 
 public class MyAppointActivity extends AppCompatActivity {
+Button b1;
+Intent backmyapp;
 
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_appoint2);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+b1= findViewById(R.id.Backfrommyappoitment);
 
+b1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        backmyapp = new Intent (MyAppointActivity.this, Main2Activity.class);
+        startActivity(backmyapp);
+    }
+});
     }
 }
