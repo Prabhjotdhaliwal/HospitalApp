b7ddd217fa2e9c8b000cab078fb3ef1f52217a0e
nnvsek <nnvsek@gmail.com>
2020-02-15 00:19:54 -0500

    check in form

--- app/src/main/java/com/example/hospitalapp/CheckInActivity.java
+++ app/src/main/java/com/example/hospitalapp/CheckInActivity.java
@@ -2,15 +2,43 @@ package com.example.hospitalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckInActivity extends AppCompatActivity {
+Button b1,b2,b3;
+Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+b1 = findViewById(R.id.PTC);
+b2 = findViewById(R.id.MA);
+        b3 = findViewById(R.id.Print);
 
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
