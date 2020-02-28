38039b7342844d85bf1e9c2ce529fb746f4dd955
Prabhjotdhaliwal <48627214+Prabhjotdhaliwal@users.noreply.github.com>
2020-02-28 18:58:44 -0500

    Merge branch 'master' of https://github.com/Prabhjotdhaliwal/HospitalApp
                                                                        # Conflicts:
                                                                        #	app/src/main/java/com/example/hospitalapp/CheckInActivity.java
                                                                        #	app/src/main/java/com/example/hospitalapp/MyAppointActivity.java

--- app/src/main/java/com/example/hospitalapp/SignUpActivity.java
+++ app/src/main/java/com/example/hospitalapp/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.example.hospitalapp;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,24 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
 
